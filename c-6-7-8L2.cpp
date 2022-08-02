76ca3df38ae90d7ccfc6c30df6d134921c13ce09
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-02 14:42:57 +0700

    Описание функции

--- c-6-7-8L2.cpp
+++ c-6-7-8L2.cpp
@@ -1,15 +1,55 @@
 
+//Глобальная область видимости
+//int a = 0;
+
+//a + a; Нельзя
+//function1();Нельзя
+
+int function1() // 
+{
+	//область видимости функции function1
+	int a = 10;
+	int b = 20;
+
+
+	return a + b;
+
+}
+
 
 
 int main() // main - особенная (указывает начальную точку входа)
 {
+	const int h = 10000;
+	char t = h;
+
+	//h = 234234;
+
+
+	1.4334;
+
+	long long w = 3255435e10;
+
+	int aa = w;
+
+	long long k = 10000000000000;
+
+	k++;
+	k--;
+
+	int k2 = k;
+
+	char a;
+	a = function1();
+
+	//область видимости функции main
 	int amontChildren;
 	int b;
 	//float d;
 	long long name;
 
 	unsigned char q;
-	char ch = 97; //0 127    97 - a
+	char ch3 = 97; //0 127    97 - a
 	// - 128 рус
 
 	'a';
@@ -24,16 +64,9 @@ int main() // main - особенная (указывает начальную точку входа)
 
 	//wchar 
 
+	function1();
 
-
-
-
-
-
-
-
-
-
+	function1();
 
 
 
