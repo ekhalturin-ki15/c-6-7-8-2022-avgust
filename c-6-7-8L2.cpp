13c372a7c7258ed4dd8e1d4ed8be4c951831eca2
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-02 16:01:37 +0700

    Объявление функций и глобальные переменные

--- c-6-7-8L2.cpp
+++ c-6-7-8L2.cpp
@@ -1,9 +1,38 @@
-
+char toUpper(char ch);
 //Глобальная область видимости
 //int a = 0;
 
 //a + a; Нельзя
 //function1();Нельзя
+
+
+
+int b;
+char ch;
+
+
+//int toUpper(bool a) // Так как название и аргументы полностью идентичны с другой
+//{
+//	return a + 1e2;
+//}
+
+
+
+
+
+
+int main() // main - особенная (указывает начальную точку входа) // Аргументы берутся при вызове с консоли
+{
+	int a = ch;
+
+	toUpper('a');
+
+
+
+}
+
+
+
 
 int My_crazy_sum(int a, int b, char ch)
 {
@@ -24,30 +53,7 @@ char toUpper(char ch, char a)
 
 char toUpper(bool a)
 {
+	int z = b;
+
 	return a + 1e2;
-}
-
-//int toUpper(bool a) // Так как название и аргументы полностью идентичны с другой
-//{
-//	return a + 1e2;
-//}
-
-
-
-int main() // main - особенная (указывает начальную точку входа)
-{
-	int t = 1200;
-
-	int a = My_crazy_sum(1000, t, 'A');
-
-	int b = My_crazy_sum(1, 5.00, 10) + My_crazy_sum(2, 2, 2);
-
-
-	char ch2 = toUpper('m');
-
-	ch2 = toUpper('m', 'a');
-
-	//ch2 = toUpper(0.343434); //Не может определиться
-
-	ch2 = toUpper(23 && 1);
 }
