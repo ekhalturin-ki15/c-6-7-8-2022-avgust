59e50437c6f86151a425f6a52e0a98d0b6d6a115
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-02 14:21:49 +0700

    Повтор

---
+++ c-6-7-8L2.cpp
@@ -0,0 +1,40 @@
+
+
+
+int main() // main - особенная (указывает начальную точку входа)
+{
+	int amontChildren;
+	int b;
+	//float d;
+	long long name;
+
+	unsigned char q;
+	char ch = 97; //0 127    97 - a
+	// - 128 рус
+
+	'a';
+	'0';
+
+	char ch = '0';
+	char ch2 = ch;
+	"s";
+	"dsdsdsdsd";
+
+	//на нельзя '2324324324'
+
+	//wchar 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
