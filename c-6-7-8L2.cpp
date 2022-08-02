89f8369470320b0f7028287d183395576f299c88
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-02 15:11:11 +0700

    Преобразование типов данных и круглые скобки

--- c-6-7-8L2.cpp
+++ c-6-7-8L2.cpp
@@ -24,6 +24,34 @@ void q() // Процедура
 int main() // main - особенная (указывает начальную точку входа)
 {
 
+	100 + 324234; // int
+
+	100 + 45435.;
+
+	4535345435435345; // long long
+
+	int a = -1 << 20; // Бит знака не участвует в сдвигах
+
+	int e = 0x400'00FF;
+
+	unsigned int qq = e;
+
+	e = unsigned int (e) << 20;
+
+
+	float f = 0.2323;
+
+
+	int aaaa = int(f) + int(f) + int(f) + int(f) + int(f) + int(f) + int(f) + int(f) + f + f + f + f + f;
+
+
+	int qqq = (1 - 2) * 32;
+
+
+	int qqqqqq = (1 - 2) * 32.0;
+	//Одно и тоже, что
+	int qqqqqq2 = (1 - 2) * float(32);
+
 	//int name = q();
 	q();
 
@@ -34,12 +62,12 @@ int main() // main - особенная (указывает начальную точку входа)
 
 	long long error = 100 * 1000000000; // переполнение
 
-	long long f = 100 * 1000000000ll;
+	//long long f = 100 * 1000000000ll; // ll на конце
 
-	f = 100 * 1000000000ull;
-	f = 100 * 1000000000u;
+	//f = 100 * 1000000000ull;
+	//f = 100 * 1000000000u;
 
-	int a = sum();
+	int aaaaa = sum();
 
 
 	sum() + sum();
