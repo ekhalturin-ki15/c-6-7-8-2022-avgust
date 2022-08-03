800757ba32ca0d09f04f0f71e3b56e9e66b4d04d
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-03 16:20:24 +0700

    Цикл while

--- C-6-7-8L3.cpp
+++ C-6-7-8L3.cpp
@@ -12,20 +12,65 @@ int a;
 int main()
 {
 
-	int a, b;
+	//while ( true )
 
-crazy_laybe:
-	scanf("%i %i", &a, &b);
 
-	if (a != b)
+	int a = 0;
+	int b = 0;
+
+
+	int i = 0;
+
+	while (i < 1e7)
 	{
-		printf("Try again    %i  %c %ull \n", 23, 32 , a);
-		goto crazy_laybe; // Ассемблер
+		b += 1;
+		i++;
 	}
 
 
 
-	printf("We all exit\n");
+	//while (true) // 10 миллионов операций в секунду
+	//{
+
+	//	//printf("%i\n", i);
+	//	//i = i + 11 / 10;
+	//	i += 11 / 10;
+	//	 //*= /= %= &=
+
+
+	//}
+
+
+	/*while (a != b)
+	{
+		printf("Number is not equal, try again\n");
+		scanf("%i %i", &a, &b);
+	}*/
+
+
+	//while (true)
+	//{
+	//	scanf("%i %i", &a, &b);
+
+	//	if (a != b)
+	//	{
+	//		printf("Number is not equal, try again\n");
+	//	}
+	//	else
+	//	{
+	//		printf("Number is equal");
+	//		break;
+	//	}
+	//} // Возврат обратно на условие
+
+
+
+
+
+	
+
+
+
 
 
 
