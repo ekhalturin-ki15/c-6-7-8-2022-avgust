879b0ebf702db4438ddf9ae37e8cc1fc56917304
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-03 14:36:20 +0700

    Адреса

---
+++ C-6-7-8L3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long LL = 24234324;
+
+	long long * andres_crazy_LL;
+
+	long long** adres_adres = &andres_crazy_LL;
+
+
+	char ch = '2';
+
+	char* adr = &ch; // 228
+
+	andres_crazy_LL = &LL;
+
+
+	cout << &LL << " ";
+	cout << andres_crazy_LL << "\n";
+	cout << ( *andres_crazy_LL) << " ";
+	cout << &andres_crazy_LL << " ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//cout << &ch;
+
+
+
+}
