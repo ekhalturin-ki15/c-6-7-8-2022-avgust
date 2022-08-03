5bf516f69ec6d86db128626c9bbadcb24944e75b
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-03 14:52:10 +0700

    Псевдонимы

--- C-6-7-8L3.cpp
+++ C-6-7-8L3.cpp
@@ -4,42 +4,48 @@ using namespace std;
 
 int main()
 {
-	long long LL = 24234324;
-
-	long long * andres_crazy_LL;
-
-	long long** adres_adres = &andres_crazy_LL;
-
-
-	char ch = '2';
-
-	char* adr = &ch; // 228
-
+	long long LL;
+	long long* andres_crazy_LL;
+	// (long long*) (long long)
 	andres_crazy_LL = &LL;
 
-
-	cout << &LL << " ";
-	cout << andres_crazy_LL << "\n";
-	cout << ( *andres_crazy_LL) << " ";
-	cout << &andres_crazy_LL << " ";
+	LL = 2323;
 
 
+	cout << *andres_crazy_LL << "    " << LL << '\n'; // Всегда совпадут
 
 
+	cout << (long long) (andres_crazy_LL) << " " << andres_crazy_LL << '\n';;
 
 
-
-
-
-
-
-
-
-
+	//long long* a; // Указатели
+	//long long* b;
 
 
 	//cout << &ch;
 
+	int a = 5;
 
+
+	cout << a << " " << *(&a) << " "<< &a << '\n';;
+
+	// int &
+	int& b = a;
+
+	cout << a << " " << b;
+
+	a++;
+
+
+	cout << a << " " << b;
+
+
+	int* bb = &a;
+	*(bb);
+
+
+
+	int& bbb = a; // Чтобы не писать звёздоски (второе имя переменной)
+	bbb;
 
 }
