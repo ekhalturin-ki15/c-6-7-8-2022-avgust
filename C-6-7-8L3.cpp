3828354d8e0c60ef3bb0344fcdd1da42fb01f485
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-03 15:33:35 +0700

    scanf printf

--- C-6-7-8L3.cpp
+++ C-6-7-8L3.cpp
@@ -1,70 +1,43 @@
 ﻿#include <iostream>
+#include <cstdio> // #include "stdio.h"
+
+#pragma warning(disable : 4996)
 
 using namespace std;
 
 
-//int sum(int* a, int bbbbbb)
-//{ 
-//	(*a) = 0; // Чтобы получить переменную из другой функции и изменить её (то есть, получаем оригинал)
-//
-//	return (*a) + bbbbbb;
-//}
-
-
-int sum(int& name_2_a, int bbbbbb)
-{ 
-	name_2_a = 0; // Чтобы получить переменную из другой функции и изменить её (то есть, получаем оригинал)
-
-	return name_2_a + bbbbbb;
-
-	//Любые изменения с name_2_a затронут и переменную 'a' из функции main
+void zeroing(int& a, int b) // Нельзя делать псевдоним на временные const
+{
+	a = 0; 
+	b = 0;
 }
+
 
 int main()
 {
 
-	int a = 8, b = 10;
+	int a = 0, b;
+
+	char ch;
+	//scanf("%c %i %ll",  ((char *)(&a)) + 3, &b);
+
+	scanf_s("%d %d", (&a), &b);
+
+
+
+	zeroing(a, b);
+
+	zeroing(a, 100);
+
+
+	//printf("%c%d   %ll %i", 213312, 's', ch, b);
+
+	printf("%d %d", a, b);
+
+	//printf();
+
+
 	//cin >> a >> b;
-
-	cout << sum(b, a) <<" ";
-
-
-	cout << a << " " << b;
-
-//	int a;
-//
-//	cout << &a << '\n';
-//
-//
-//	int b;
-//
-//	cin >> a >> b;
-//
-//
-//	//cout << sum(&a, b) <<'\n';
-//
-//	cout << sum(a, b) << '\n';
-//
-//	cout << a << " " << b;
-//
-//
-//
-//
-//	//*-> int
-//
-//	char* adr; // Указатель
-//
-//	char ch = 'a';
-//
-//	char& ch2 = ch; //Псевдоним
-//
-//	adr = &ch;
-//
-//	//cout << int(adr) + 5;
-
-
-
-
-
+	//cout << a << " " << b;
 
 }
