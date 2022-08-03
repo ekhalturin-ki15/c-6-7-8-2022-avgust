b9aa273b5ff3c6407927d6ee09cca9801c1e6ab8
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-03 15:15:26 +0700

    Псевдонимы в функциях

--- C-6-7-8L3.cpp
+++ C-6-7-8L3.cpp
@@ -3,48 +3,64 @@
 using namespace std;
 
 
-int sum(int* a, int bbbbbb)
+//int sum(int* a, int bbbbbb)
+//{ 
+//	(*a) = 0; // Чтобы получить переменную из другой функции и изменить её (то есть, получаем оригинал)
+//
+//	return (*a) + bbbbbb;
+//}
+
+
+int sum(int& name_2_a, int bbbbbb)
 { 
-	(*a) = 0; // Чтобы получить переменную из другой функции и изменить её (то есть, получаем оригинал)
+	name_2_a = 0; // Чтобы получить переменную из другой функции и изменить её (то есть, получаем оригинал)
 
-	return (*a) + bbbbbb;
+	return name_2_a + bbbbbb;
+
+	//Любые изменения с name_2_a затронут и переменную 'a' из функции main
 }
-
-
-
-
-
 
 int main()
 {
-	int a;
 
-	cout << &a << '\n';
+	int a = 8, b = 10;
+	//cin >> a >> b;
 
+	cout << sum(b, a) <<" ";
 
-	int b;
-
-	cin >> a >> b;
-
-
-	cout << sum(&a, b) <<'\n';
 
 	cout << a << " " << b;
 
-
-
-
-	//*-> int
-
-	char* adr; // Указатель
-
-	char ch = 'a';
-
-	char& ch2 = ch; //Псевдоним
-
-	adr = &ch;
-
-	//cout << int(adr) + 5;
+//	int a;
+//
+//	cout << &a << '\n';
+//
+//
+//	int b;
+//
+//	cin >> a >> b;
+//
+//
+//	//cout << sum(&a, b) <<'\n';
+//
+//	cout << sum(a, b) << '\n';
+//
+//	cout << a << " " << b;
+//
+//
+//
+//
+//	//*-> int
+//
+//	char* adr; // Указатель
+//
+//	char ch = 'a';
+//
+//	char& ch2 = ch; //Псевдоним
+//
+//	adr = &ch;
+//
+//	//cout << int(adr) + 5;
 
 
 
