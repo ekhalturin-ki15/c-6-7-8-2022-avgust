84f19681aed8b3ba0ef22906fbefccfcd9e538d0
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-04 17:10:00 +0700

    Используем динамические массивы

--- L4!.cpp
+++ L4!.cpp
@@ -75,91 +75,72 @@ int main()
     freopen("input.txt", "r", stdin); // stdin - это консольный ввод
     freopen("output.txt", "w", stdout);  // stdout - это консольный вывод
 
+    int h;
 
-    std::string name_smail;//Не надо delete
-
-    cin >> name_smail;
-    
-
-    //std::string name_smail;
-
-    //cin >> name_smail;
-
-    //char* name_smail;
-
-    //int size_name;
-    //cin >> size_name;
-    //name_smail = new char[size_name + 1];
-
-    //for (int i = 0; i < size_name; ++i)
-    //{
-    //    cin >> name_smail[i];
-    //}
-    //name_smail[size_name] = '\0';
-
-
-  
-
-    cin >> h >> w;
-
-    //char a[h][w]; // y  x
-
-    //char* a = new char[h * w];
+    cin >> h;
     char** a = new char* [h];
-    for (int y = 0; y < h; ++y)
-        a[y] = new char[w];
+    int* size_a = new int[h];
 
-
-    //system("pause");
 
     for (int y = 0; y < h; ++y)
     {
-        for (int x = 0; x < w; ++x)
+        int now_size;
+        cin >> now_size;
+        size_a[y] = now_size;
+        a[y] = new char[ now_size ];
+
+        for (int x = 0; x < now_size; ++x)
         {
-            a[y][x] = '.';
-            if (y == h / 3)
-                if ((x == w / 5) || (x == 4 * w / 5))
-                {
-                    a[y][x] = '0';
-                }
+            cin >> a[y][x];
         }
-
-        if (y == 8 * h / 10)
-        {
-            for (int x = w / 10; x <= 9 * w / 10; ++x)
-            {
-                a[y][x] = '_';
-            }
-
-            a[y][w / 10] = '\\';
-            a[y][9 * w / 10] = '/';
-
-        }
-
     }
 
-   /* int i = 0;
-    while (name_smail[i] != '\0')
-        cout << name_smail[i++];*/
-
-    cout << name_smail;
 
     for (int y = 0; y < h; ++y)
     {
+        int craaaazy_yy;
+        if ((y & 1) == 0)
+        {
+            craaaazy_yy = y + 1;
+        }
+        else
+        {
+            craaaazy_yy = y - 1;
+        }
+
+
+            
+
+        for (int x = 0; x < size_a[craaaazy_yy]; ++x)
+        {
+            cout << a[craaaazy_yy][x];
+        }
         cout << "\n";
-        for (int x = 0; x < w; ++x)
-        {
-            cout << a[y][x];
-        }
+
 
     }
 
 
-    
-    for (int y = 0; y < h; ++y)
-        delete [] a[y];
 
-    delete[] a;
+
+
+    /*char*** a = new char**[g];
+    for (int z = 0; z < g; ++z)
+    {
+        a[z] = new char*[h];
+        for (int y = 0; y < h; ++y)
+        {
+            a[z][y] = new char [w];
+        }
+    }*/
+
+
+
+
+
+
+
+
 
 
     _CrtDumpMemoryLeaks();
