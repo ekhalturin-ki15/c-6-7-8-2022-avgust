f676dfc63fefeb940956446b0bb9daa641a69248
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-08 14:25:15 +0700

    Структуры

--- L6.cpp
+++ L6.cpp
@@ -7,20 +7,103 @@
 //G++ #include "bits/stdc++.h"
 using namespace std;
 
+#pragma warning(disable : 4996)
+
+struct CrazyStruct
+{
+	int x;
+	int y;
+
+	char ch;
+
+	int i[1000];
+
+
+}   a1, a2;
+
+int i1, i2;
+
+
+struct MyPoint
+{
+	int x;
+	int y;
+	int z;
+
+	string name;
+
+}  p1, p2;
+
+
+int dis_between_point(MyPoint l, MyPoint r)
+{
+	//cin >> l;
+	//cin >> l.x >> l.y >> l.z;
+	return l.x + l.y + l.z - r.x - r.y - r.z;
+}
+
+int dis_between_point(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+	return x1 + y1 + z1 - x2 - y2 - z2;
+}
+
+
+
 int main()
 {
-	cout << setprecision(20) << fixed << 1 / 3.;
+	FILE* IN;
+	FILE* OUT;
+	freopen_s(&IN,"input.txt", "r", stdin);
+	freopen_s(&OUT, "output.txt", "w", stdout);
 
-	cout << setw(10) << 'a' << setw(10) << 'b' << setw(10) << 'c';
+	int x1, y1, z1;
+	int x2, y2, z2;
 
-	int i(5); // int i = 5;
 
-	ifstream crazy_input("input.txt");
 
-	std::vector<int> crazy_mas(100);
+	if (1 == 2)
+	{
+		//true
 
-	crazy_mas[10] = 0;
+	}
+	else
+	{
+		//false
 
-	cout << crazy_mas[1];
+	}
+
+	while (1 == 2) // пока true выполняем фигурные скобки
+	{
+		
+
+	}
+
+	for (int i = 0; i < 100; ++i) // выполнится 100 шагов
+	{
+
+	}
+
+	int a[1000];
+
+	int N = 1000;
+	int* da = new int[N];
+
+	delete[] da;
+
+	vector<int> va(1000);
+
+	va[9] = 100;
+
+
+	p1.x = 1000;
+
+	p2.x = 2000;
+
+
+
+
+
+
+
 
 }
