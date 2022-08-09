dba05d5cc857c072bcf66ca3fcae4284c97060cd
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-09 16:55:46 +0700

    Игра

--- L7.cpp
+++ L7.cpp
@@ -1,8 +1,10 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <vector> //~20% дольше
 #include <string>
 #include <forward_list>
 #include <list> // Список
+#include <random>
 
 //G++ #include "bits/stdc++.h"
 using namespace std;
@@ -16,63 +18,200 @@ int main()
 {
 	FILE* IN;
 	FILE* OUT;
-	freopen_s(&IN, "input.txt", "r", stdin);
-	freopen_s(&OUT, "output.txt", "w", stdout);
+	//freopen_s(&IN, "input.txt", "r", stdin);
+	//freopen_s(&OUT, "output.txt", "w", stdout);
 
-	vector<int> v = {2, 3, 4, 2, 1, 2, 3, 3, 4, 4};
+	//srand(  time(0) ); // Задаём Сид рандома
+	srand( 4 );
 
-	list <int> l = { 2, 3, 4, 5, 2, 3, 4, 5, 6, 6, 2, 3 };
+	ifstream crazy_input("input.txt");
 
-	//l.resize()
+	ofstream log("output.txt");
 
-	vector<int>::iterator crazy_it;
+	list<char> mapa;
 
-	list<int>::iterator list_it;
-
-	crazy_it = v.begin();
-
-	crazy_it = v.end();
+	vector<int> pos;
 
 
-	for (crazy_it = v.begin(); crazy_it != v.end(); crazy_it += 2)
+	int N, len;
+	crazy_input >> N >> len;
+
+
+	mapa.assign(len, '#');
+
+	//rand(); // 5 10 6 7 ... 5 10 6 7
+
+	//mt19937 ma; // 
+
+	rand(); // int 0 - 
+
+	
+	int gener_outs = N;
+
+	while (gener_outs != 0)
 	{
-		cout << (*crazy_it);
+		int i = 0;
+		for (list<char>::iterator it = mapa.begin(); it != mapa.end(); ++it, ++i)
+		{
+			char& ch = *it;
+
+			int r = rand() % 2;
+
+			if ((ch == '#') && (r == 1))
+			{
+				int r2 = rand() % 5;
+
+				pos.push_back(i);
+				--gener_outs;
+
+				switch (r2)
+				{
+				case 0:
+					ch = '@';
+					break;
+				case 1:
+					ch = '?';
+					break;
+
+				case 2:
+					ch = '%';
+					break;
+
+				case 3:
+					ch = '}';
+					break;
+
+				case 4:
+					ch = '&';
+					break;
+				}
+
+			}
+
+			if (gener_outs == 0) break;
+		}
+		if (gener_outs == 0) break;
 	}
 
+	
+
+
+	int rand_head = rand() % pos.size();
+
+	list<char>::iterator it = mapa.begin();
+
+	(advance(it, pos[rand_head]));
+	
+	*it = '!';
+
+	for (list<char>::iterator it = mapa.begin(); it != mapa.end(); ++it)
+	{
+		log << *it;
+	}
+	log << "\n";
+
+
+	char ch;
+	int hp = 1000;
+
+	while ((hp > 0) && (N > 0))
+	{
+		
+		cout << hp << "\n";
+		for (list<char>::iterator it = mapa.begin(); it != mapa.end(); ++it)
+		{
+			cout << *it;
+		}
+		cout << "\n";
+		cin >> ch;
+		cout << "\n";
+		for (list<char>::iterator it = mapa.begin();
+			it != mapa.end();
+			++it)
+		{
+			//char& cit = *it;
+
+			if (*it == ch)
+			{
+				while (*it == ch)
+				{
+
+					list<char>::iterator next_it = next(it, 1); // запомнил следующего
+					mapa.erase(it);
+					--N;
+					it = next_it;
+					if (it == mapa.end()) break;
+				}
+
+				break;
+			}
+		}
+
+		for (list<char>::iterator it = mapa.begin();
+			it != mapa.end();
+			++it)
+		{
+			int r;
+			switch (*it)
+			{
+			case '@':
+				
+				r = rand() % 5;
+				if (r != 4)
+					hp -= 5;
+				break;
+
+			case '?':
+
+				r = rand() % 3;
+				if (r == 0)
+					hp -= 20;
+
+				break;
+
+			case '%':
+
+				hp -= 2;
+
+				break;
+
+			case '}':
+
+				r = rand() % 5;
+				if (r == 0)
+					hp += 2;
+				else
+					hp -= 3;
+
+				break;
+
+			case '&':
+				r = rand() % 6;
+				if (r == 0)
+					hp += 3;
+				else
+					hp -= 5;
+
+				break;
+
+			case '!':
+				hp -= 20;
+				break;
+			}
+
+
+
+		}
+
+	}
 	cout << "\n";
+	if (hp <= 0)
+		cout << "YOU DIED, game over";
+	else
+		cout << "C++ rulezz";
 
-	cout << endl; // Для общения двух программ cout.flush() очистка буфера вывода
-	//Совокупность команд//cout << "\n"; cout.flush();
 
-	for (list_it = l.begin(); list_it != l.end(); list_it++)
-	{
-		cout << (*list_it);
-	}
 
-	cout << "\n\n";
-
-	list_it = l.end();
-
-	for (int i = 0; i < l.size(); i++)
-	{
-		--list_it;
-		cout << (*list_it);	
-	}
-
-	cout << "\n\n";
-
-	for (list_it = l.end(); list_it != l.begin(); )
-	{
-		--list_it;
-		cout << (*list_it);
-	}
-	cout << "\n\n";
-	list<int>::reverse_iterator rlist_it; // Нельзя исп. обычный итератор
-
-	for (rlist_it = l.rbegin(); rlist_it != l.rend(); rlist_it++)
-	{
-		cout << (*rlist_it);
-	}
 
 	return 0;
 }
