aba918caa9756b2b83241bbeac8518fdb25919cd
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-09 14:46:33 +0700

    Композиция и Декомпозиция

--- L7.cpp
+++ L7.cpp
@@ -4,6 +4,9 @@
 #include <iomanip>
 #include <cmath>
 #include <fstream>
+#include <functional>
+
+
 //G++ #include "bits/stdc++.h"
 using namespace std;
 
@@ -16,14 +19,10 @@ struct CrazyPoint
 	CrazyT y;
 
 	string name;
+
+
 };
 
-
-template <typename CrazyT>
-CrazyT sum(CrazyT a, CrazyT b) // В функциях сам по себе определяется шаблонный тип, в зависимости от аргументов
-{
-	return a + b;
-}
 
 
 int sum(int a, int b) // В функциях сам по себе определяется шаблонный тип, в зависимости от аргументов
@@ -31,14 +30,39 @@ int sum(int a, int b) // В функциях сам по себе определ
 	return a + b;
 }
 
+//
+//string sum(string a, string b) // В функциях сам по себе определяется шаблонный тип, в зависимости от аргументов
+//{
+//	return a + b;
+//}
 
-string sum(string a, string b) // В функциях сам по себе определяется шаблонный тип, в зависимости от аргументов
+
+
+//int crazyDoing(void* fun)
+//{
+//	return *((int*)  fun);
+//
+//}
+
+int crazyDoing(std::function<int(int, int)> f);
+
+
+template <typename T, typename M>
+struct CrazyStruct
 {
-	return a + b;
-}
+	T a;
+	M d;
+
+};
 
 
+struct AAA
+{
+	int r;
+	char c;
+	double d;
 
+};
 
 int main()
 {
@@ -47,20 +71,48 @@ int main()
 	freopen_s(&IN, "input.txt", "r", stdin);
 	freopen_s(&OUT, "output.txt", "w", stdout);
 
-	vector<int> v;
-	vector<char> vv;
+	vector<int> v = { 34, 3, 3 , 2 , 1, 3, 2 }; // Композиция
 
-	CrazyPoint<int> pi;
-	CrazyPoint<double> pd;
+	CrazyStruct<int, double> cs = {100,  0};
 
-	pi.x = 10.5;
+	pair<int, double> pr = {1 , 23};
 
-	pd.x = 10.5;
+	pr.first = 100;
 
-	cout << sum<int>(32, 34.23);
+	pr.second = 0.233;
 
-	std::min(1, 2);
+	tuple<int, double, char, int> t = {1, 3.34, 'f', 0};
+	tuple<int, double, char, int> tt = t;
+
+	get<1>(t) = 10.234;
+
+	auto z = 100;
+	auto zz = 1.32323;
+
+
+	int crazy_xx = pr.first;
+
+
+	auto [crazy_x, crazy_y] = pr; // Копирование из pair
+	crazy_x = 10;
+	crazy_y = 100;
+
+
+	auto& [crazyy_x, crazyy_y] = pr; // Оригинальные поля из pair
+	crazy_x = 10;
+
+	int& crazyy_xx = pr.first;
+
+	AAA aaa;
+	aaa.c = 1;
+	aaa.d = 1;
+	aaa.r = 1;
+
+	auto [q1, q2, q3] = aaa;
+
+	aaa.r = 100;
+
+	cout << q1;
 
 	return 0;// 
-
 }
