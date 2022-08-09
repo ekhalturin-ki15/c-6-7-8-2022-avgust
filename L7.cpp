3e9e028501e140d2c01c422dba89e37111d5c08e
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-08-09 17:08:32 +0700

    Конструктор Деструктор

--- L7.cpp
+++ L7.cpp
@@ -12,206 +12,60 @@ using namespace std;
 #pragma warning(disable : 4996)
 
 
+struct CrazyPlayer
+{
+	int hp;
+	int ammo;
+	int bullet;
 
+	//CrazyPlayer() = default;
+
+	//Конструктор
+	CrazyPlayer(int start_hp)
+	{
+		hp = start_hp;
+		ammo = 100;
+		bullet = 0;
+	}
+
+	// Полностью совпадает имя структуры (параметры, которых может и не быть)
+	CrazyPlayer()//Вызывается при создании
+	{
+		hp = 100;
+		ammo = 0;
+		bullet = 1000;
+	}
+	//Полностью совпадает имя структуры , но в начале ~ (параметры, которых может и не быть)
+	~CrazyPlayer() // Вызывается при удалении объекта
+	{
+		cout << "Destruct";
+	}
+};
+
+int zero()
+{
+	{
+		CrazyPlayer crazy_object;
+	}
+
+	return 0;
+}
 
 int main()
 {
 	FILE* IN;
 	FILE* OUT;
-	//freopen_s(&IN, "input.txt", "r", stdin);
-	//freopen_s(&OUT, "output.txt", "w", stdout);
-
-	//srand(  time(0) ); // Задаём Сид рандома
-	srand( 4 );
-
-	ifstream crazy_input("input.txt");
-
-	ofstream log("output.txt");
-
-	list<char> mapa;
-
-	vector<int> pos;
-
-
-	int N, len;
-	crazy_input >> N >> len;
-
-
-	mapa.assign(len, '#');
-
-	//rand(); // 5 10 6 7 ... 5 10 6 7
-
-	//mt19937 ma; // 
-
-	rand(); // int 0 - 
+	freopen_s(&IN, "input.txt", "r", stdin);
+	freopen_s(&OUT, "output.txt", "w", stdout);
 
 	
-	int gener_outs = N;
+	CrazyPlayer crazy_object2(10);
 
-	while (gener_outs != 0)
-	{
-		int i = 0;
-		for (list<char>::iterator it = mapa.begin(); it != mapa.end(); ++it, ++i)
-		{
-			char& ch = *it;
+	CrazyPlayer* craze_ptr = new CrazyPlayer(200);
 
-			int r = rand() % 2;
+	zero();
 
-			if ((ch == '#') && (r == 1))
-			{
-				int r2 = rand() % 5;
-
-				pos.push_back(i);
-				--gener_outs;
-
-				switch (r2)
-				{
-				case 0:
-					ch = '@';
-					break;
-				case 1:
-					ch = '?';
-					break;
-
-				case 2:
-					ch = '%';
-					break;
-
-				case 3:
-					ch = '}';
-					break;
-
-				case 4:
-					ch = '&';
-					break;
-				}
-
-			}
-
-			if (gener_outs == 0) break;
-		}
-		if (gener_outs == 0) break;
-	}
-
-	
-
-
-	int rand_head = rand() % pos.size();
-
-	list<char>::iterator it = mapa.begin();
-
-	(advance(it, pos[rand_head]));
-	
-	*it = '!';
-
-	for (list<char>::iterator it = mapa.begin(); it != mapa.end(); ++it)
-	{
-		log << *it;
-	}
-	log << "\n";
-
-
-	char ch;
-	int hp = 1000;
-
-	while ((hp > 0) && (N > 0))
-	{
-		
-		cout << hp << "\n";
-		for (list<char>::iterator it = mapa.begin(); it != mapa.end(); ++it)
-		{
-			cout << *it;
-		}
-		cout << "\n";
-		cin >> ch;
-		cout << "\n";
-		for (list<char>::iterator it = mapa.begin();
-			it != mapa.end();
-			++it)
-		{
-			//char& cit = *it;
-
-			if (*it == ch)
-			{
-				while (*it == ch)
-				{
-
-					list<char>::iterator next_it = next(it, 1); // запомнил следующего
-					mapa.erase(it);
-					--N;
-					it = next_it;
-					if (it == mapa.end()) break;
-				}
-
-				break;
-			}
-		}
-
-		for (list<char>::iterator it = mapa.begin();
-			it != mapa.end();
-			++it)
-		{
-			int r;
-			switch (*it)
-			{
-			case '@':
-				
-				r = rand() % 5;
-				if (r != 4)
-					hp -= 5;
-				break;
-
-			case '?':
-
-				r = rand() % 3;
-				if (r == 0)
-					hp -= 20;
-
-				break;
-
-			case '%':
-
-				hp -= 2;
-
-				break;
-
-			case '}':
-
-				r = rand() % 5;
-				if (r == 0)
-					hp += 2;
-				else
-					hp -= 3;
-
-				break;
-
-			case '&':
-				r = rand() % 6;
-				if (r == 0)
-					hp += 3;
-				else
-					hp -= 5;
-
-				break;
-
-			case '!':
-				hp -= 20;
-				break;
-			}
-
-
-
-		}
-
-	}
-	cout << "\n";
-	if (hp <= 0)
-		cout << "YOU DIED, game over";
-	else
-		cout << "C++ rulezz";
-
-
-
+	delete craze_ptr;
 
 	return 0;
 }
